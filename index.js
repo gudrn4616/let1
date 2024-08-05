9d94e0f68933448a12bfe326b5a5cc5bd9f431f8
gudrn4616 <128803700+gudrn4616@users.noreply.github.com>
2024-08-05 14:56:55 +0900

    메인 홈페이지

--- index.js
+++ index.js
@@ -9,15 +9,7 @@ var app = express();
 
 
 app.get('/', function(req, res) {
-    const data = `<div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>`
-    res.send(main.header() + data + main.end());
+    res.send(main());
 });
 
 app.listen(3000);
