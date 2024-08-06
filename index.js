5f712b79636080ba0ce45a41c0cda6df269410fd
gudrn4616 <128803700+gudrn4616@users.noreply.github.com>
2024-08-06 18:02:22 +0900

    main branch 금지

--- index.js
+++ index.js
@@ -145,6 +145,6 @@ app.put('/api/guestbook/:id', (req, res) => {
 });
 
 // 서버를 3000번 포트에서 실행
-app.listen(3000, function() {
-    console.log('서버가 3000번 포트에서 실행 중입니다.');
+app.listen(5000, ()=> {
+    console.log('서버가 5000번 포트에서 실행 중입니다.');
 });
