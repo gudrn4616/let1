9e9cbbee9a96959139cd4257bc6d079b51b25e94
gudrn4616 <128803700+gudrn4616@users.noreply.github.com>
2024-08-06 10:46:09 +0900

    수정

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
 app.get('/', function(req, res) {
-    var indexhtml = path.join(__dirname, 'index.html');
+    var indexhtml = path.join(__dirname, 'html/index.html');
     fs.readFile(indexhtml, 'utf8', function(err, data) {
         if (err) {
             res.status(500).send('파일을 읽는 중 오류가 발생했습니다.');
@@ -17,7 +17,7 @@ app.get('/', function(req, res) {
 
 app.get('/kimhyunggu', function(req, res) {
     
-    var introductionPath = path.join(__dirname, '자기소개.html');
+    var introductionPath = path.join(__dirname, 'html/자기소개.html');
     fs.readFile(introductionPath, 'utf8', function(err, data) {
         if (err) {
             res.status(500).send('파일을 읽는 중 오류가 발생했습니다.');
