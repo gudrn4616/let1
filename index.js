2a9bf6f1acfcb94363b5a3aad3656cabec228f8e
gudrn4616 <128803700+gudrn4616@users.noreply.github.com>
2024-08-07 16:10:52 +0900

    수정

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var app = express();
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) =>{
     var indexhtml = path.join(__dirname, 'html/index.html');
     fs.readFile(indexhtml, 'utf8', function(err, data) {
         if (err) {
@@ -15,7 +15,7 @@ app.get('/', function(req, res) {
     });
 });
 
-app.get('/Kim', function(req, res) {
+app.get('/Kim', (req, res) =>{
     var introductionPath = path.join(__dirname, 'html/자기소개.html');
     fs.readFile(introductionPath, 'utf8', function(err, data) {
         if (err) {
@@ -26,7 +26,7 @@ app.get('/Kim', function(req, res) {
     });
 });
 
-app.get('/Heo', function(req, res) {
+app.get('/Heo', (req, res) =>{
     var introductionPath = path.join(__dirname, 'html/Heo.html');
     fs.readFile(introductionPath, 'utf8', function(err, data) {
         if (err) {
@@ -37,7 +37,7 @@ app.get('/Heo', function(req, res) {
     });
 });
 
-app.get('/Mun', function(req, res) {
+app.get('/Mun', (req, res) =>{
     var introductionPath = path.join(__dirname, 'html/Mun.html');
     fs.readFile(introductionPath, 'utf8', function(err, data) {
         if (err) {
@@ -48,6 +48,28 @@ app.get('/Mun', function(req, res) {
     });
 });
 
-app.listen(3000, function() {
+app.get('/Lyu', (req, res) =>{
+    var introductionPath = path.join(__dirname, 'html/Lyu.html');
+    fs.readFile(introductionPath, 'utf8', function(err, data) {
+        if (err) {
+            res.status(500).send('파일을 읽는 중 오류가 발생했습니다.');
+        } else {
+            res.send(data);
+        }
+    });
+}); 
+
+app.get('/Lee', (req, res) =>{
+    var introductionPath = path.join(__dirname, 'html/Lee.html');
+    fs.readFile(introductionPath, 'utf8', function(err, data) {
+        if (err) {
+            res.status(500).send('파일을 읽는 중 오류가 발생했습니다.');
+        } else {
+            res.send(data);
+        }
+    });
+}); 
+
+app.listen(3000, ( )=> {
     console.log('서버가 3000번 포트에서 실행 중입니다.');
 });
